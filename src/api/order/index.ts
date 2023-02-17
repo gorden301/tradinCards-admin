c3806b97b9bad5a23947911c5ea5c3a428c5b55f
gorden301 <1019108859@qq.com>
2023-02-17 17:28:05 +0800

    feat: 修改权限，修改vite proxy，增加koa调用获取订单列表方法

---
+++ src/api/order/index.ts
@@ -0,0 +1,11 @@
+import { request } from "@/utils/service"
+
+const baseUrl = 'http://localhost:3000/'
+/** 所有订单列表 */
+export function getOrderList() {
+  return request({
+    // baseUrl,
+    url: "order/orderList",
+    method: "get"
+  })
+}
